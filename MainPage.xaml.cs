c192622bc4915a1c640512fdec27a238965e0053
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-21 21:59:49 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
